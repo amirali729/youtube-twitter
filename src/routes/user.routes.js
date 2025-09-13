1be78e670556e2dbb11488768f3e86b4fde57737
amirali729 <amirpechs10@gmail.com>
2025-09-13 16:58:24 +0500

    add user-related routes for profile management and history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken }  from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, updateUserDetails,changeUserPassword, getUserProfile,updateUserAvatar,updateUserCoverImage,getUserChannelProfile, getUserWatchHistory }  from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -22,5 +22,13 @@ router.route("/auth/login").post(loginUser)
 
 router.route("/auth/logout").post(verifyJwt,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/auth/change-password").post(verifyJwt,changeUserPassword)
+router.route("/get-user").get(verifyJwt,getUserProfile)
+router.route("/update-user-details").patch(verifyJwt,updateUserDetails)
+router.route("/update-user-avatar").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+router.route("/update-user-coverImage").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/channel/:userName").get(verifyJwt,getUserChannelProfile)
+router.route("/history").get(verifyJwt,getUserWatchHistory)
 
 export default router
