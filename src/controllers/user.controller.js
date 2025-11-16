9cc4b66ed49b6dd572d57de06e237437c48bcb10
amirali729 <amirpechs10@gmail.com>
2025-11-16 19:01:16 +0500

    fixed minor bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
-import { ApiError } from "../utils/ApiError.js"
+import { ApiError } from "../utils/apiError.js"
 import { User } from '../models/user.models.js'
 import { fileUploading } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
